3ecdc68d338df860319ae39d447d560fc6b9294e
Gardener <81845773+Gardener-soul@users.noreply.github.com>
2025-04-26 21:47:05 +0900

    Revert "Design/router navigation/2"

--- FE/src/navigation/StackNavigator.tsx
+++
@@ -1,9 +0,0 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-const Stack = createNativeStackNavigator();
-
-export default function StackNavigator() {
-  return (
-    <StackNavigator screenOptions={{ headerShown: false }}></StackNavigator>
-  );
-}
