5f41bfc19a1ed74572337eb1848a9a2f19e6e9ee
Gardener-Soul <str-pes@naver.com>
2025-04-29 23:23:07 +0900

    [Chore] BE Project 구축
    1. 기본 Spring Initializer를 통해서 제작
    ++ 이게 지금 잘 된건가,,

---
+++ BE/src/test/java/com/watseo/be/BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watseo.be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
